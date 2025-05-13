5f72993ba8084ec6f7dee4443575af2e16ac32d1
Michal356 <michal356566@gmail.com>
2025-05-13 20:24:21 +0200

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
+import { Helmet } from 'react-helmet';
 
 import React from "react";
 
 export default function Home() {
   return (
+      
+      <Helmet>
+        <title>Wynajem Pokoi i Mieszkań | Roomino – Komfortowe Zakwaterowanie w Twoim Mieście</title>
+        <meta name="description" content="Szukasz mieszkania lub pokoju do wynajęcia? Roomino to nowoczesna platforma z komfortowymi ofertami najmu. Szybka rezerwacja online. Sprawdź dostępność!" />
+      </Helmet>
+
+
     <div>
       <div className="hero">
         <h1>Roomino</h1>
