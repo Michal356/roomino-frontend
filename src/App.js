8be73f80ffc241555309fdf95dd6c2748c7a03d7
Michal356 <michal356566@gmail.com>
2025-05-13 21:17:38 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import AdminPanel from './pages/AdminPanel';
 import Rooms from './pages/Rooms';
+import Dashboard from './pages/Dashboard';
+import WhyRoomino from './pages/WhyRoomino';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Rooms />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/why" element={<WhyRoomino />} />
+          <Route path="/rooms" element={<Rooms />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/admin" element={<AdminPanel />} />
-          <Route path="/rooms" element={<Rooms />} />
         </Routes>
       </Router>
     </AuthProvider>
